bcf2e1c36d653e9b9824d1a369fdb3c4a3bd5c0f
Eduardo Hernandez Cuamatzi <terratent@hotmail.com>
2023-04-27 15:31:47 -0600

    Fix for payload format

--- src/main/java/com/tacriptosso/criptodatasso/encriptData.java
+++ src/main/java/com/tacriptosso/criptodatasso/encriptData.java
@@ -1,6 +1,5 @@
 package com.tacriptosso.criptodatasso;
 import java.io.IOException;
-import java.util.Base64;
 import java.util.HashMap;
 import com.pingidentity.opentoken.Agent;
 import com.pingidentity.opentoken.TokenException;
@@ -17,8 +16,12 @@ public class encriptData {
             userInfo.put("SAML_SUBJECT", dataModel.criptoData.SAML_SUBJECT);
             userInfo.put("firstName", dataModel.dataCripto.firstName);
             userInfo.put("lastName", dataModel.dataCripto.lastName);
-            userInfo.put("payload", Base64.getEncoder().encodeToString((dataModel.dataCripto.payload).getBytes()));
+            userInfo.put("payload", dataModel.dataCripto.payload);
+            //userInfo.put("payload", "Complete payload goes here- demo with SalesForce app-dev team....");
             token = agent.writeToken(userInfo);
+            String baseURL = "https://fedmdl.transamerica.com/idp/startSSO.ping?PartnerSpId=ta%3Ademo%3Amo%3Aopentoken%3Asp";
+            String ssoURL = baseURL + "&opentoken=" + token;
+            System.out.println("SSO URL:\n" + ssoURL);
         } catch (TokenException e) {
             e.printStackTrace();
         }
